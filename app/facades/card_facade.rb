4ead71eacae5cb0a2c71a01fa14104ced51feac2
Isaac Alter <i_alter@yahoo.com>
2023-09-19 13:12:52 -0600

    Add feat: facade being built and can get results by name

--- app/facades/card_facade.rb
+++ app/facades/card_facade.rb
@@ -1,10 +1,38 @@
 class CardFacade
   attr_reader :card_name
 
+  # Will initialize with a card name
   def initialize(card_name)
     @card_name = card_name
   end
 
-  
+  # This method will return a hash of cards that match the card name
+  def card_name_search
+    cards = service.get_cards_by_name(@card_name)
+  end
 
+  # This method will first check to see if there is only one card that matches the card name. If there is only one card,
+  # it will return that card. If there is more than one card, it will send that hash of cards to another method.
+  def card_name_results
+    results = card_name_search[:cards]
+
+    if results.size == 1
+      return results.first
+    elsif results.size > 1
+      set_names = retrieve_set_name(results)
+    else
+      return nil
+    end
+  end
+
+  def retrieve_set_names(cards)
+    cards.map do |card|
+      card[:set]
+    end.uniq
+  end
+
+  private
+  def service
+    @service ||= CardService.new
+  end
 end
--- spec/facades/card_facade_spec.rb
+++ spec/facades/card_facade_spec.rb
@@ -1,7 +1,19 @@
 require "rails_helper"
 
 RSpec.describe CardFacade do
+  # Creates a new facade instance for each test
   before :each do
-    
+    @test_facade = CardFacade.new("Avacyn, Angel of Hope")
+  end
+
+  describe "instance methods" do
+    it "can get a specific card with the proper name", :vcr do
+      expect(@test_facade.card_name_search).to be_a(Hash)
+      expect(@test_facade.card_name_search).to have_key(:cards)
+      expect(@test_facade.card_name_search[:cards]).to be_an(Array)
+      expect(@test_facade.card_name_search[:cards].size).to eq(11)
+      expect(@test_facade.card_name_search[:cards][0]).to have_key(:name)
+      expect(@test_facade.card_name_search[:cards][1][:name]).to eq("Avacyn, Angel of Hope")
+    end
   end
 end
